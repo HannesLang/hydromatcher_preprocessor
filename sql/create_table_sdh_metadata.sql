83c48ebc3bb91e1eee90f821f6b89d618c979af9
HannesLang <johannes.lang@mobi.ch>
2018-12-10 16:40:56 +0100

    db sequence added

--- sql/create_table_sdh_metadata.sql
+++ sql/create_table_sdh_metadata.sql
@@ -1,10 +1,19 @@
+-- SEQUENCE: public.t_sdh_metadata_id_seq
+
+-- DROP SEQUENCE public.t_sdh_metadata_id_seq;
+
+CREATE SEQUENCE public.t_sdh_metadata_id_seq;
+
+ALTER SEQUENCE public.t_sdh_metadata_id_seq
+    OWNER TO postgres;
+
 -- Table: public.t_sdh_metadata
 
 -- DROP TABLE public.t_sdh_metadata;
 
 CREATE TABLE public.t_sdh_metadata
 (
-    id integer NOT NULL DEFAULT nextval('"T_DSH_Metadata_id_seq"'::regclass),
+    id integer NOT NULL DEFAULT nextval('"t_sdh_metadata_id_seq"'::regclass),
     qmax integer NOT NULL,
     shapefile_tablename character varying(50) COLLATE pg_catalog."default",
     qvol integer NOT NULL,
@@ -19,3 +28,4 @@ ALTER TABLE public.t_sdh_metadata
     OWNER to postgres;
 COMMENT ON TABLE public.t_sdh_metadata
     IS 'Ganglinien-Metadaten';
+commit;
